4fdfdc6dec29b1a6247672b3e18b5d5eb17db1ad
rdsubhas <rdsubhas@gmail.com>
2014-07-16 16:42:48 +0300

    Renamed sunspot rake namespace

--- site-cookbooks/seed/recipes/default.rb
+++ site-cookbooks/seed/recipes/default.rb
@@ -29,7 +29,7 @@ execute "rake-app-migrate" do
 end
 
 execute "sunspot-start" do
-  command "su vagrant -l -c 'cd /vagrant && bundle exec rake sunspot:clean_start'"
+  command "su vagrant -l -c 'cd /vagrant && bundle exec rake search:clean_start'"
 end
 
 execute "scheduler-start" do
