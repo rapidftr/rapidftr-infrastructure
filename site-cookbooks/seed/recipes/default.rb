1f0e851200bfb3763d357a9fc823fd74237c3817
rdsubhas <rdsubhas@gmail.com>
2014-07-21 15:12:50 +0300

    Use sunspot:solr rake tasks instead of custom search: tasks

--- site-cookbooks/seed/recipes/default.rb
+++ site-cookbooks/seed/recipes/default.rb
@@ -29,7 +29,7 @@ execute "rake-app-migrate" do
 end
 
 execute "sunspot-start" do
-  command "su vagrant -l -c 'cd /vagrant && bundle exec rake search:clean_start'"
+  command "su vagrant -l -c 'cd /vagrant && bundle exec rake sunspot:solr:restart'"
 end
 
 execute "scheduler-start" do
