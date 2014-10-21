7c311091cda1820400b713f99cf7f102f65fadec
Charles Tumwebaze <ctumwebaze@gmail.com>
2014-10-21 11:43:32 +0300

    cleanup the setup script for building the windows installer.

--- installer/winbuild.sh
+++ installer/winbuild.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-vagrant reload --provision linux-dev && vagrant reload --provision linux-test
+vagrant up --provision linux-dev && vagrant up --provision linux-test
 vagrant halt linux-dev && vagrant halt linux-test && VBoxManage export winrapidftr -o windows/winrapidftr.ova
 
-vagrant reload --provision linux-windows-installer && vagrant halt linux-windows-installer
+vagrant up --provision linux-windows-installer && vagrant halt linux-windows-installer
 
 
 
